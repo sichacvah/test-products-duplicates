a1b0b4aedfeab694c502864876d758f31c97aa90
Sergey Kurochkin <sergey.kurochkin@instamart.ru>
2021-10-19 16:21:48 +0500

    init

---
+++ cli.js
@@ -0,0 +1,3 @@
+import config from './config.js'
+import { main } from './index.js'
+main(config)
---
+++ index.js
@@ -0,0 +1,58 @@
+import fetch from 'node-fetch'
+
+
+const fetchPage = (baseUrl, config, page = 1) => {
+  console.log('PAGE_FETCHING', page)
+  const queryString = `sid=${config.sid}&tid=${config.tid}&per_page=${config.per_page}&page=${page}`
+  const url = `${baseUrl}?${queryString}`
+  return fetch(url, {
+    method: 'get',
+    headers: config.headers
+  }).then(response => {
+    return response.json()
+  })
+}
+
+const extractIds = (products) => {
+  return products.map(p => p.id)
+}
+
+const findDuplicates = (idsSet, ids) => {
+  return ids.filter(id => idsSet.has(id))
+}
+
+const addToIds = (idsSet, ids) => {
+  ids.forEach(id => {
+    idsSet.add(id)
+  })
+}
+
+export const main = (config) => {
+  const idsSet = new Set()
+  const duplicates = []
+  let lastPage = 1
+  const url = 'https://' + config.host + config.path
+  const fetchAndExtract = (pageNumber = 1) => {
+    return fetchPage(url, config, pageNumber).then(page => {
+      const ids = extractIds(page.products)
+      duplicates.push(...findDuplicates(idsSet, ids))
+      addToIds(idsSet, ids)
+      const nextPage = page.meta.next_page
+      if (nextPage === lastPage) throw new Error(`Cycle nextPage - ${nextPage} equals lastPage - ${lastPage}`)
+      if (nextPage) {
+        lastPage = nextPage
+        return fetchAndExtract(nextPage)
+      }
+      return Promise.resolve(duplicates)
+    })
+  }
+  const fetchPages = () => {
+    fetchAndExtract().then(items => {
+      console.log('All ids set:', idsSet)
+      console.log('Found duplicates ids:', items)
+    })
+  }
+
+  fetchPages()
+}
+
